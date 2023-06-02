98899bb9dea9aa637363e4bf52ea5b37bdd19336
sadettinak <sadettinakman@gmail.com>
2023-06-02 14:44:42 +0300

    Initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,27 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+<<<<<<< HEAD
+=======
+        byte a= 100;
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Byte.MIN_VALUE);
+        short b=100;
+        System.out.println(Short.MAX_VALUE);
+        System.out.println(Short.MIN_VALUE);
+        long c=100;
+        System.out.println(Long.MAX_VALUE);
+        System.out.println(Long.MIN_VALUE);
+        short J=1000;
+        int i=(J/2);
+        System.out.println(i);
+       // byte short int long
+        short d=100;
+        byte m=2;
+        int k=4;
+        long n=d+m+k;
+        System.out.println(d);
+
+>>>>>>> f35eb33 (Initial commit)
     }
 }
